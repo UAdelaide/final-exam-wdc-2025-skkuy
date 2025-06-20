6cb5c68fbd5324c712376bddbbaa33aa195c5375
zskuy <songkeyan947@gmail.com>
2025-06-20 07:13:11 +0000

    [automatic commit] 2025-06-20 07:13:11 UTC

--- part1/app.js
+++ part1/app.js
@@ -41,9 +41,11 @@ let db;
       password: '',
       database: 'DogWalkService'
     });
-      } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
+
+
+    } catch (err) {
+     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+    }
 })();
 
 // Route to return books as JSON
