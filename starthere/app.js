833bce283c94b75aac64a7c8b0ffecc2cab5c0e0
zskuy <songkeyan947@gmail.com>
2025-06-20 07:34:05 +0000

    [automatic commit] 2025-06-20 07:34:05 UTC

--- starthere/app.js
+++ starthere/app.js
@@ -166,9 +166,8 @@ app.get('/api/dogs', async(req, res) => {
 app.get('/api/walkrequests/', async(req, res) => {
     try {
         const [rows] = await db.execute(`
-            SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            FROM Dogs d
-            JOIN Users u ON d.owner_id = u.user_id
+          
+
         `);
         res.json(rows);
 
