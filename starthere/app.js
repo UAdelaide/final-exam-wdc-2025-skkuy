4c0a4c576169da125efe590eb33894dc06398dd8
zskuy <songkeyan947@gmail.com>
2025-06-20 07:40:35 +0000

    [automatic commit] 2025-06-20 07:40:35 UTC

--- starthere/app.js
+++ starthere/app.js
@@ -174,7 +174,7 @@ app.get('/api/walkrequests/open', async(req, res) => {
             wr.loction,
             u.username AS owner_username
           FROM WalkRequests wr
-          JOIN Dogs
+          JOIN Dogs d ON wr.dog_id
 
 
         `);
