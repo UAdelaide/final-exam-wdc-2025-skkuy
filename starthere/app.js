ee456e2ff8d4c808426d8b10416afa261a41565a
zskuy <songkeyan947@gmail.com>
2025-06-20 07:28:16 +0000

    [automatic commit] 2025-06-20 07:28:16 UTC

--- starthere/app.js
+++ starthere/app.js
@@ -156,7 +156,7 @@ app.get('/api/dogs', async(req, res) => {
         res.json(rows);
 
     } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: ' to fetch books' });
     }
 
 });
