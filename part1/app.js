ef6ed46401d0a12336398efafd7d5195b90c1cd1
zskuy <songkeyan947@gmail.com>
2025-06-20 07:03:57 +0000

    [automatic commit] 2025-06-20 07:03:57 UTC

--- part1/app.js
+++ part1/app.js
@@ -36,13 +36,14 @@ let db;
 
     // Create a table if it doesn't exist
     await db.execute(`
-        CREATE TABLE Dogs (
-            dog_id INT AUTO_INCREMENT PRIMARY KEY,
-            owner_id INT NOT NULL,
-            name VARCHAR(50) NOT NULL,
-            size ENUM('small', 'medium', 'large') NOT NULL,
-            FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-        )
+      CREATE TABLE IF NOT EXISTS Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
     `);
 
     await db.execute(`
@@ -52,7 +53,7 @@ let db;
             name VARCHAR(50) NOT NULL,
             size ENUM('small', 'medium', 'large') NOT NULL,
             FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-        );
+        )
     `);
 
 
