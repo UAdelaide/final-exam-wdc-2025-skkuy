30040f19ccff3cf84a21e255e493ad843c29589c
zskuy <songkeyan947@gmail.com>
2025-06-20 07:47:06 +0000

    [automatic commit] 2025-06-20 07:47:06 UTC

--- starthere/app.js
+++ starthere/app.js
@@ -166,18 +166,18 @@ app.get('/api/dogs', async(req, res) => {
 app.get('/api/walkrequests/open', async(req, res) => {
     try {
         const [open] = await db.execute(`
-      SELECT
-        wr.request_id,
-        d.name AS dog_name,
-        wr.requested_time,
-        wr.duration_minutes,
-        wr.location,
-        u.username AS owner_username
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
-      ORDER BY wr.requested_time
+          SELECT
+            wr.request_id,
+            d.name AS dog_name,
+            wr.requested_time,
+            wr.duration_minutes,
+            wr.location,
+            u.username AS owner_username
+          FROM WalkRequests wr
+          JOIN Dogs d ON wr.dog_id = d.dog_id
+          JOIN Users u ON d.owner_id = u.user_id
+          WHERE wr.status = 'open'
+          ORDER BY wr.requested_time
         `);
         res.json(open);
 
