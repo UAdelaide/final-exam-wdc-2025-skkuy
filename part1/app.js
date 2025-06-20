e11a44655d1dca297c02f77ed79f8b89fdcb02aa
zskuy <songkeyan947@gmail.com>
2025-06-20 07:11:07 +0000

    [automatic commit] 2025-06-20 07:11:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -41,24 +41,8 @@ let db;
       password: '',
       database: 'DogWalkService'
     });
-
-
-
-
-    // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
-    if (rows[0].count === 0) {
-      await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
-      `);
-    }
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
-})();
+  }}
+);
 
 // Route to return books as JSON
 app.get('/', async (req, res) => {
