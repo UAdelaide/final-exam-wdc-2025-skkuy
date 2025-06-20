6b0775fe4b477a90ea5c34d58952d2fda605f9cf
zskuy <songkeyan947@gmail.com>
2025-06-20 07:17:58 +0000

    [automatic commit] 2025-06-20 07:17:58 UTC

--- part1/app.js
+++ part1/app.js
@@ -72,7 +72,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // /api/dogs
 app.get('/api/dogs', async(req, res) => {
     try {
-        const [rows] = await.db.execute(`
+        const [rows] = await db.execute(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
             FROM Dogs d
             JOIN Users u ON d.owner_id = u.user_id
@@ -81,6 +81,7 @@ app.get('/api/dogs', async(req, res) => {
 
     } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
+    }
 
 });
 
