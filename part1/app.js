b821a54bdfbf5de1ad34978aee4d633ff89bb28f
zskuy <songkeyan947@gmail.com>
2025-06-20 07:17:38 +0000

    [automatic commit] 2025-06-20 07:17:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ app.get('/api/dogs', async(req, res) => {
         `);
         res.json(rows);
 
-    }} catch (err) {
+    } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
 
 });
