c2281c7aaa99617ae10e3d21a6c94b998572dc8c
zskuy <songkeyan947@gmail.com>
2025-06-20 07:20:43 +0000

    [automatic commit] 2025-06-20 07:20:43 UTC

--- part1/app.js
+++ part1/app.js
@@ -100,7 +100,7 @@ let db;
 
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    const [userRows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO books (title, author) VALUES
