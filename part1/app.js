a24c8e13501c0663398edd5da13ff24f57cbb08b
zskuy <songkeyan947@gmail.com>
2025-06-20 07:14:12 +0000

    [automatic commit] 2025-06-20 07:14:12 UTC

--- part1/app.js
+++ part1/app.js
@@ -42,8 +42,13 @@ let db;
       database: 'DogWalkService'
     });
 
-    await db.execute(
-        `
+    await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('zzz', 'zzz@example.com', 'hashed110', 'owner'),
+('fff', 'fff@example.com', 'hashed120', 'walker');
         `
     )
 
