e7f463633bc8fce837df6a0fcc009958e620a349
zskuy <songkeyan947@gmail.com>
2025-06-20 07:35:48 +0000

    [automatic commit] 2025-06-20 07:35:48 UTC

--- starthere/app.js
+++ starthere/app.js
@@ -166,6 +166,7 @@ app.get('/api/dogs', async(req, res) => {
 app.get('/api/walkrequests/open', async(req, res) => {
     try {
         const [rows] = await db.execute(`
+          SEL
 
 
         `);
