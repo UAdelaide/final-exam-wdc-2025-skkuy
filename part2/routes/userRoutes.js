cca412c06b56fa059d96265c570cf0efeba56dbb
zskuy <songkeyan947@gmail.com>
2025-06-21 00:06:59 +0000

    [automatic commit] 2025-06-21 00:06:59 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,7 +51,7 @@ router.post('/login', async (req, res) => {
 
     const user = rows[0];
 
-    res.json({ message: 'Login successful', user: rows[0] });
+    res.json({ message: 'Login successful', user: user });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
